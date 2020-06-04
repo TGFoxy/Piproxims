47ef6efc2c645d076f337810b2891f6fae4dd446
TGFoxy <38375754+TGFoxy@users.noreply.github.com>
2020-06-04 17:18:28 +0100

    Update PiSquaredOver6.py

--- PiSquaredOver6.py
+++ PiSquaredOver6.py
@@ -1,9 +1,11 @@
 import math
 n = 1
 s = 0
-for n in range(1, 99999999999999999999999999999999999999999999999999999999999999999):
+Range = int(input("Range"))
+f = open("Data Revised.txt","a+")
+for n in range(1, Range):
     s  += 1/n**2
-    print(s, end="\r")
 x = s*6
 pisquare = math.sqrt(x)
-print("Pi is ", pisquare)
+f.write("Pi is " + str(pisquare) + "\n")
+f.close()
